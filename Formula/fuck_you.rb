b96f939add95ed12d56be400bad5f0e4e75c8740
baxromumarov <baxromumarov10@gmail.com>
2025-01-27 16:52:51 +0100

    add formula

--- Formula/fuck_you.rb
+++ Formula/fuck_you.rb
@@ -2,7 +2,7 @@ class FuckYou < Formula
   desc "A description of your Go project"
   homepage "https://github.com/Baxromumarov/fuck-you"
   url "https://github.com/Baxromumarov/fuck-you/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "db3c98b0484863334ef28829e5da42a2195de1b1daf0e482e931e3e79e8f91ba"
   license "MIT" # Or your project's license
 
   depends_on "go" => :build
