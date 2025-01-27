e7ca6d1dfcb7a33ed19a48b35dc5435fb8650213
baxromumarov <baxromumarov10@gmail.com>
2025-01-27 17:19:17 +0100

    v4

--- Formula/fuck_you.rb
+++ Formula/fuck_you.rb
@@ -1,9 +1,8 @@
 class FuckYou < Formula
   desc "A description of your Go project"
-  homepage "https://github.com/Baxromumarov/fuck-you"
-  url "https://github.com/Baxromumarov/fuck-you/archive/refs/tags/v1.0.4.tar.gz"
+  homepage "https://github.com/Baxromumarov/homebrew-fuck-you"
+  url "https://github.com/Baxromumarov/homebrew-fuck-you/archive/refs/tags/v1.0.4.tar.gz"
   sha256 "7e1afb2077cae2fdd62f751803cd6c8d21fe5f292c4c1b40c22b910d2f27c38d"
-  license "MIT"
 
   depends_on "go" => :build
 
