ef1062e1e7237824628858509ed6753599eee503
baxromumarov <baxromumarov10@gmail.com>
2025-01-27 17:20:42 +0100

    v5

--- Formula/fuck_you.rb
+++ Formula/fuck_you.rb
@@ -1,16 +1,18 @@
 class FuckYou < Formula
   desc "A description of your Go project"
   homepage "https://github.com/Baxromumarov/homebrew-fuck-you"
-  url "https://github.com/Baxromumarov/homebrew-fuck-you/archive/refs/tags/v1.0.4.tar.gz"
+  url "https://github.com/Baxromumarov/homebrew-fuck-you/archive/refs/tags/v1.0.5.tar.gz"
   sha256 "7e1afb2077cae2fdd62f751803cd6c8d21fe5f292c4c1b40c22b910d2f27c38d"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    cd "cmd" do  
+      system "go", "build", "-o", bin/"fuck_you", "."
+    end
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/fuck-you --help")
+    assert_match "Usage", shell_output("#{bin}/fuck_you --help")
   end
 end
